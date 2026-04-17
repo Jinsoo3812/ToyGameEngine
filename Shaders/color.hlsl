64841bf8d70cb2ab29d837ec2ffeb58c74c962c1
Kim Kwan Hee <vlvkrhksgml3812@naver.com>
2026-04-17 23:06:57 +0900

    HLSL 파일 정리

--- Shaders/color.hlsl
+++ Shaders/color.hlsl
@@ -9,15 +9,17 @@ cbuffer cbPerObject : register(b0)
 	float4x4 gWorldViewProj; 
 };
 
+// 정점 셰이더의 Input Signature
 struct VertexIn
 {
 	float3 PosL  : POSITION;
     float4 Color : COLOR;
 };
 
+// 정점 셰이더의 Output Signature
 struct VertexOut
 {
-	float4 PosH  : SV_POSITION;
+	float4 PosH  : SV_POSITION; // SV_는 시스템 값을 나타내는 시맨틱. Rasterrizer는 이 시맨틱을 보고 위치를 계산해야 한다.
     float4 Color : COLOR;
 };
 
@@ -25,10 +27,11 @@ VertexOut VS(VertexIn vin)
 {
 	VertexOut vout;
 	
-	// Transform to homogeneous clip space.
+	// 입력 받은 3차원 좌표(vin.PosL)을 동차 좌표로 변환
+	// 이후 WVP 행렬과 곱하여 동차 클립 공간에서의 좌표로 변환
 	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
 	
-	// Just pass vertex color into the pixel shader.
+	// 색상은 그대로 출력
     vout.Color = vin.Color;
     
     return vout;
@@ -36,6 +39,7 @@ VertexOut VS(VertexIn vin)
 
 float4 PS(VertexOut pin) : SV_Target
 {
+	// Rasterizer를 거친 pixel의 색상을 그대로 출력
     return pin.Color;
 }
 
